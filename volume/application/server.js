457f8d6edd076b637143e23449351a79e55014b8
arbuzinside <arbuzinside@gmail.com>
2015-05-10 20:35:37 +0300

    spaceify added

--- volume/application/server.js
+++ volume/application/server.js
@@ -1,9 +1,9 @@
-var express    = require('express');
+var express = require('express');
 var path = require('path');
-var app        = express();
+var app  = express();
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'www')));
 
 var router = express.Router();
 router.use(function(req, res, next) {
